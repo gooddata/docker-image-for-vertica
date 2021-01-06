71479e84842e0618ac27e77f262ac367b41736c7
jaceksan <jan.soubusta@gooddata.com>
2021-01-06 09:12:50 +0100

    Vertica docker images
    CentOS and Debian OS families
    Generation and load of VMART schema

---
+++ build.sh
@@ -0,0 +1,91 @@
+#!/usr/bin/env bash
+
+set -e
+
+function show_help() {
+  cat << EOF
+Usage: $0 -v <vertica version> -f <OS family> -o <OS version> [-r <docker repository>]
+Options are:
+  -v - Vertica version, e.g. 10.0.1-5
+  -f - OS family, e.g. CentOS, Debian
+  -o - OS version of base image, e.g. 7.9.2009, 8.3.2011, ubuntu:18.04
+  -r - Image name prefix, a path to repository, e.g. 123456789012.dkr.ecr.eu-central-1.amazonaws.com/databases
+  -p - Push built image into repository defined by the image name prefix (-r)
+  -h - show help
+EOF
+}
+
+function report_invalid_arg() {
+   echo "Missing mandatory $1 option"
+   echo
+   show_help
+}
+
+# A POSIX variable
+OPTIND=1 # Reset in case getopts has been used previously in the shell.
+
+REPOSITORY=""
+VERTICA_VERSION=""
+OS_FAMILY=""
+OS_VERSION=""
+VERTICA_IMAGE=""
+PUSH_IMAGE="n"
+
+while getopts "h?v:f:o:r:p" opt; do
+    case "$opt" in
+    h)  show_help
+        exit 0
+        ;;
+    \?) echo "Invalid option: -$OPTARG" >&2
+        echo
+        show_help
+        exit 1
+        ;;
+    v)  VERTICA_VERSION="${OPTARG}"
+        ;;
+    f)  OS_FAMILY="${OPTARG}"
+        ;;
+    o)  OS_VERSION="${OPTARG}"
+        ;;
+    r)  REPOSITORY="${OPTARG}"
+        ;;
+    p)  PUSH_IMAGE="y"
+        ;;
+    esac
+done
+
+shift $((OPTIND-1))
+
+if [ -z "${VERTICA_VERSION}" ]; then
+  report_invalid_arg "-v"
+  exit 1
+fi
+
+if [ -z "${OS_VERSION}" ]; then
+  report_invalid_arg "-o"
+  exit 1
+fi
+
+if ! ls packages/vertica?${VERTICA_VERSION}* 1>/dev/null 2>&1; then
+  echo "No Vertica package file exists in \"packages\" folder for Vertica version ${VERTICA_VERSION}"
+  exit 1
+fi
+
+IMAGE_VERSION=$(echo "${VERTICA_VERSION}.${OS_FAMILY}_${OS_VERSION}" | sed -r 's/[^a-z0-9_.-]/_/gi')
+
+if [ -z "${REPOSITORY}" ]; then
+  VERTICA_IMAGE="vertica:${IMAGE_VERSION}"
+else
+  VERTICA_IMAGE="${REPOSITORY}/vertica:${IMAGE_VERSION}"
+fi
+
+echo "Building image $VERTICA_IMAGE ...."
+docker build -f Dockerfile_${OS_FAMILY} \
+  --build-arg vertica_version=${VERTICA_VERSION} \
+  --build-arg os_version=${OS_VERSION} \
+  -t $VERTICA_IMAGE .
+
+if [ "${PUSH_IMAGE}" == "y" ]; then
+  echo "Pushing $VERTICA_IMAGE to repository ...."
+  docker push $VERTICA_IMAGE
+fi
---
+++ vmart/02_vmart_etl.sql
@@ -0,0 +1,13 @@
+-- Add timestamp columns into facts tables
+-- Most of use cases can utilize timestamp columns and corresponding SQL functions instead of joining date_dimension table.
+-- Joining date_dimension table has significant negative impact on performance
+-- Utilize flatten tables everywhere it works
+
+alter table online_sales.online_sales_fact add column online_sales_saledate DATE DEFAULT (SELECT date FROM public.date_dimension WHERE online_sales.online_sales_fact.sale_date_key = public.date_dimension.date_key);
+alter table online_sales.online_sales_fact add column online_sales_shipdate DATE DEFAULT (SELECT date FROM public.date_dimension WHERE online_sales.online_sales_fact.ship_date_key = public.date_dimension.date_key);
+alter table public.inventory_fact add column inventory_date DATE DEFAULT (SELECT date FROM public.date_dimension WHERE public.inventory_fact.date_key = public.date_dimension.date_key);
+alter table store.store_sales_fact add column store_sales_date DATE DEFAULT (SELECT date FROM public.date_dimension WHERE store.store_sales_fact.date_key = public.date_dimension.date_key);
+
+alter table store.store_sales_fact add column store_sales_datetime TIMESTAMP;
+update store.store_sales_fact set store_sales_datetime = to_date(to_char(store_sales_date, 'yyyy-mm-dd')||' '||to_char(transaction_time, 'hh24:mi:ss'), 'yyyy-mm-dd hh24:mi:ss');
+commit;
